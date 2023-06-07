22910f135dfca90fb7987017e082d7575bf9cf50
RJ <rjay.tedoco@gmail.com>
2023-06-07 14:32:17 +0800

    Add upload modal - Allow authenticated users to upload songs with image covers

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import { TbPlaylist } from "react-icons/tb";
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const { user } = useUser();
+  const uploadModal = useUploadModal();
+
   const onClick = () => {
-    // TODO: Handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // TODO: Check for subscription
+
+    return uploadModal.onOpen();
   };
 
   return (
